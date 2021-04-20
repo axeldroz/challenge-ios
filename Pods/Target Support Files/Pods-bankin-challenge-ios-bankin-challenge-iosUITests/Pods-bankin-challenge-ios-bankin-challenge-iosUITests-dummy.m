5e56ea99e202da6c8103bbc9083d7ea80617f573
Axel Drozdzynski <axeldroz@hotmail.fr>
2021-04-20 20:44:23 +0200

    remove Pods; add .gitignore

--- Pods/Target Support Files/Pods-bankin-challenge-ios-bankin-challenge-iosUITests/Pods-bankin-challenge-ios-bankin-challenge-iosUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_bankin_challenge_ios_bankin_challenge_iosUITests : NSObject
-@end
-@implementation PodsDummy_Pods_bankin_challenge_ios_bankin_challenge_iosUITests
-@end
